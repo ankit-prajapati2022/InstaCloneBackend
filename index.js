019a4f7944a23ccff382d1ddd2b28387fd616c0d
Ankit~ <ankitkp028@gmail.com>
2022-12-05 17:11:24 +0530

    error

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(cookies());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
 
