b5bc61213bf8abda51a560d7cf55b166d914f492
Ankit~ <ankitkp028@gmail.com>
2022-12-06 10:41:29 +0530

    Updating

--- index.js
+++ index.js
@@ -16,14 +16,13 @@ app.use(cookies());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-  cors({
-    origin: "https://instaclonebitcs.netlify.app/",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api", authRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
