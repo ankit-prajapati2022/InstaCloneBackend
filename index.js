9c909d3a0a43bfeaa85b662158d64e8de01d63f8
Ankit Prajapati <ankitkp028@gmail.com>
2022-12-07 16:43:30 +0530

    Trial

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-const dotenv = require("dotenv");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const cookies = require("cookie-parser");
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// const cookies = require("cookie-parser");
 const express = require("express");
 const app = express();
 
-const authRoute = require("./routes/api/auth");
+// const authRoute = require("./routes/api/auth");
 
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
+// dotenv.config();
 
-app.use(cookies());
+// app.use(cookies());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
+// app.use(cors());
 
-app.use("/api", authRoute);
+// app.use("/api", authRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Successfully connected to database"))
-  .catch((error) => {
-    console.log("[-] Mongoose error");
-    console.log(error);
-  });
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("Successfully connected to database"))
+//   .catch((error) => {
+//     console.log("[-] Mongoose error");
+//     console.log(error);
+//   });
 
-const server = require("http").createServer(app);
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
